d0f5d324d5cd60bdd2f24406a70c2d9a578c2bfb
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-10-01 21:36:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,261 @@ var prefix = "c:";
 bot.on('ready', () => {
   bot.user.setGame('c:help for commands');
   console.log(`Kitilen initilized.`);
+  client.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help") {
+  const embed = new Discord.RichEmbed()  
+      .setColor("RANDOM")
+      .setDescription(`
+     
+             Please Select Your Language
+${prefix}help-ar
+${prefix}help-en
+             
+      `)
+   message.channel.sendEmbed(embed)
+   
+   }
+   });
+ 
+   bott.on("message", message => {
+ if (message.content === "c:help-ar") {
+  const embed = new Discord.RichEmbed()  
+      .setColor("RANDOM")
+      .setDescription(`
+     
+            اختر:
+ 
+##help-gn-ar ⇏ اوامر عامة
+##help-ad-ar ⇏ اوامر ادارة السيرفر
+             
+##help-mu-ar ⇏ اوامر الموسيقى
+`)
+message.channel.sendEmbed(embed)
+ 
+}
+});
+ 
+bot.on("message", message => {
+    if (message.content === "c:help-en") {
+     const embed = new Discord.RichEmbed()  
+         .setColor("RANDOM")
+         .setDescription(`
+         
+              Chose:
+               
+   ##help-gn-en ⇏ General commands
+   
+   ##help-ad-en ⇏ Server management commands
+               
+   ##help-mu-en ⇏ Music commands
+   
+   
+   `)
+   message.channel.sendEmbed(embed)
+   
+   }
+   });
+ 
+   bot.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help-gn-ar") {
+     message.channel.send('**تم ارسال رسالة في الخاص** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`
+             
+===================== اوامر عامة =====================
+##id ➾ معلومات عن حسابك
+##ping ➾ سرعة اتصالك بالانترنت
+##avatar ➾ يظهر صورة بروفابلك
+##server ➾ معلومات عن السيرفر
+##bot ➾ معلومات عن البوت
+##credit ➾ يظهرلك الكردت حقق
+##daily ➾ يومية
+##trans [@someone] [number] ➾ لتحويل كردت لشخص اخر
+##count ➾ يعرضلك عدد الاشخاص الي بالسيرفر
+##short ➾ يختصرلك الروابط
+##say ➾ يكرر كلامك
+##image ➾ صورة السيرفر
+##contact ➾ لمراسله صاحب البوت
+##invites ➾ يعرضلك عدد انفايتاتك بالسيرفر
+##inv ➾ رابط اذخال البوت
+##support ➾ سيرفر الدعم
+=========================================================
+وقريباً المزيد من الاكواد
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+ 
+ 
+ 
+ 
+ 
+   bott.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help-gn-en") {
+     message.channel.send('**Check your dm** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`
+             
+==================== General commands =====================
+##id ➾ your informations
+##ping ➾ your ping
+##avatar ➾ your profile avatar
+##server ➾ server informations
+##bot ➾ bot informations
+##credit ➾ your credits
+##daily ➾ your daily credits
+##trans [@mention] [number] ➾ to transfer credits for someone
+##count ➾ server members without bots
+##short ➾ shorten links
+##say ➾ repeat your words
+##image ➾ server image
+##contact ➾ send text to bot owner
+##invites ➾ to see your invites
+##inv ➾ bot invite link
+##support ➾ support server
+=========================================================
+More commands soon
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+ 
+   bot.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help-ad-ar") {
+     message.channel.send('**تم ارسال رسالة بالخاص** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`
+             
+==================== اوامر ادارية =====================
+##bc ➾ لارسال رساله لجميع الاعضاء
+##ban [@mention] [reason] ➾  لحظر شخص من السيرفر
+##kick [@mention] [reason] ➾ لطرد شخص من السيرفر
+##mute [@mention] [reason] ➾ لاعطاء ميوت لعضو
+##unmute [@mention] ➾ لفك الميوت عن عضو
+##move [@mention] ➾ لنقل عضو لرومك الصوتي
+##ccolors [number] ➾ لصنع عدد من الالوان
+##mutechannel ➾ لاقفال الشات
+##unmutechannel ➾ لفتح الشات
+##clear ➾ لمسح الشات
+##role humans [role name] ➾ لعطاء رتبة للاشخاص فقط
+##role bots [role name] ➾ لاعطاء رتبة للبوتات فقط
+##role [@mention] [role name] ➾ لاعطاء رتبة لعضو
+##hchannel ➾ لاخفاء الشات
+##schannel ➾ لاظهار الشات
+=========================================================
+وقريباً المزيد من الاكواد
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+   message.author.sendEmbed(embed)
+   
+}
+});
+ 
+bot.on("message", message => {
+ var prefix = "c:";
+if (message.content === "c:help-ad-en") {
+  message.channel.send('**Check your dm** :mailbox_with_mail: ');
+const embed = new Discord.RichEmbed()
+   .setColor("RANDOM")
+   .setDescription(`
+         
+==================== Management commands =====================
+##bc ➾ for massage send message to server members
+##ban [@mention] [reason] ➾ to ban someone from the server
+##kick [@mention] [reason] ➾ to kick someone from the server
+##mute [@mention] [reason] ➾ to mute someone
+##unmute [@mention] ➾ to umnute someone
+##move [@mention] ➾ to move someone to your channel
+##ccolors [number] ➾ to create colors
+##mutechannel ➾ to mute chat
+##unmutechannel ➾ to ummute chat
+##clear ➾ to clear chat
+##role humans [role name] ➾ to give role for humans only
+##role bots [role name] ➾ to give role for bots only
+##role [@mention] [role name] ➾ to give role fo someone
+##hchannel ➾ to hide chat
+##schannel ➾ to show chat
+=========================================================
+More commands soon
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+message.author.sendEmbed(embed)
+ 
+}
+});
+ 
+ 
+bot.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help-mu-ar") {
+     message.channel.send('**تم ارسال رسالة بالخاص** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`
+             
+==================== اوامر الميوزك =====================
+##play ➾ لتشغيل اغنية
+##skip ➾ لتخطي اغنية
+##pause ➾ لإيقآف الأغنية مؤقتا
+##resume ➾ لتشغيل الاغننية
+##vol ➾ لتغير مستوى الصوت 0 - 100
+##stop ➾ لاخراج البوت من الروم
+##np ➾ لمعرفة الاغنية مشغلة
+##queue ➾ قائمة الاغاني
+ 
+=========================================================
+وقريباً المزيد من الاكواد
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+   message.author.sendEmbed(embed)
+   
+}
+});
+ 
+ 
+bot.on("message", message => {
+    var prefix = "c:";
+ if (message.content === "c:help-mu-en") {
+     message.channel.send('**Check your dm** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setDescription(`
+             
+==================== Music commands =====================
+##play ➾ to play song
+##skip ➾ to skip song
+##pause ➾ to pause the song
+##resume ➾ To resume the song
+##vol ➾ to change the volume 0 - 100
+##stop ➾ top remove the bot from room
+##np ➾ to show the song that is currently playing
+##queue ➾ to see the song list
+ 
+=========================================================
+More codes soon
+=========================================================
+Support server : https://discord.gg/cUgYbEr
+`)
+   message.author.sendEmbed(embed)
+   
+}
+});
   bot.on('guildMemberAdd', Sal => { //By Salto7#4595
     var embed = new Discord.RichEmbed()
     .setAuthor(Sal.user.username, Sal.user.avatarURL)
@@ -751,10 +1006,7 @@ bot.on('message', msg => {
      msg.channel.send("Ping! (hold on, processing latency...)").then(m => m.edit(`Ping! (Current latency is ${m.createdTimestamp - msg.createdTimestamp}ms, while the API Latency is ${Math.round(bot.ping)}ms)`) );
 
   }
-  if (command === "help") {
-    msg.channel.sendMessage(":calling: It seems you have requested help. Check your DMs.");
-    msg.author.sendMessage("Thanks for using the help command this command will help you know the current commands.c:ping and c:pong are commands used to check if the bot is online.c:say allows you to make the bot say whatever you want it to say.c:calculateadd is an adding calculator.c:8ball is a fun command where you can ask the magic 8 ball a question and it will reply.c:invite makes the bot DM you an invite link to invite the bot to your server. And c:objection, c:holdit and c:takethat are AA commands. c:server gives you every peice of information you need to know about your server,c:date gives you a full date .c:bc allows you to send every user a costum message,and leave and join messages are also available.,c:bot bot info,c:server server info ,do c:dtchannel to delete a text channel,plus an autoresponse system and an antiswear system, do c:add-swear to add one,do c:remove-swear to remove one,and c:remove-all-swears,do c:swears for a list of corrunt added swears, do c:acheive to register an acheivement ")
-  }
+ 
 
   if (command === "objection") {
     msg.channel.sendMessage(`http://i.imgur.com/19WEQFO.gif ${args.join(" ")}`);
